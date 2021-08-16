6ad3cebe4109ac8e58bbd228524bef973a8f367a
dmitry krokhin <nekufa@gmail.com>
2021-08-16 16:58:05 +0300

    metrics operations api

---
+++ src/Metrics/Operations.php
@@ -0,0 +1,39 @@
+<?php declare(strict_types=1);
+
+namespace Basis\Telemetry\Metrics;
+
+class Operations
+{
+    private array $data = [];
+    private array $labels = [];
+
+    public static function create(array $labels = []): self
+    {
+        return new self($labels);
+    }
+
+    public function __construct(array $labels = [])
+    {
+        $this->labels = $labels;
+    }
+
+    public function increment(string $key, float | int $amount = 1, array $labels = []): self
+    {
+        $this->data[] = [ 'increment', $key, $amount, $labels ];
+        return $this;
+    }
+
+    public function set(string $key, float | int $value, array $labels = []): self
+    {
+        $this->data[] = [ 'set', $key, $value, $labels ];
+        return $this;
+    }
+
+    public function apply(Registry $registry): self
+    {
+        foreach ($this->data as [ $method, $key, $value, $labels ]) {
+            $registry->$method($key, $value, array_merge($this->labels, $labels));
+        }
+        return $this;
+    }
+}
---
+++ tests/Metrics/OperationsTest.php
@@ -0,0 +1,34 @@
+<?php declare(strict_types=1);
+
+namespace Tests\Metrics;
+
+use Basis\Telemetry\Metrics\Operations;
+use Basis\Telemetry\Metrics\Registry;
+use PHPUnit\Framework\TestCase;
+
+class OperationsTest extends TestCase
+{
+    public function testBasics(): void
+    {
+        $registry = new Registry();
+
+        $operations = Operations::create([ 'hostname' => 1 ])
+            ->increment('request_counter')
+            ->increment('request_memory', memory_get_peak_usage(true))
+            ->increment('request_timing', 42)
+            ->increment('processing_counter', 1, [ 'queue' => 'tester', ])
+            ->increment('todo_counter', -1, [ 'queue' => 'tester', ])
+            ->set('activity_timestamp', time());
+
+        $operations->apply($registry);
+
+        $this->assertSame(1, $registry->get('request_counter', [ 'hostname' => 1 ]));
+        $this->assertSame(-1, $registry->get('todo_counter', [ 'hostname' => 1, 'queue' => 'tester' ]));
+
+        // operations should be serializable
+        unserialize(serialize($operations))->apply($registry);
+
+        $this->assertSame(2, $registry->get('request_counter', [ 'hostname' => 1 ]));
+        $this->assertSame(42 * 2, $registry->get('request_timing', [ 'hostname' => 1 ]));
+    }
+}
